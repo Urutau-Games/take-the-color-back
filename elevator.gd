7bcc81e67304a873ed9525426013361e15ae0e50
Lazaro Menezes <lazarodm@gmail.com>
2026-03-19 14:05:03 -0300

    Objects prototypes

---
+++ elevator.gd
@@ -0,0 +1,58 @@
+extends AnimatableBody2D
+
+@export var highest_floor: int = 5
+@export var lowest_floor: int = 1
+@export var elevator_speed: int = 5
+@onready var door: StaticBody2D = $Door
+
+const floor_size: float = 430
+
+var _is_player_safe: bool = false
+var _is_moving: bool = false
+
+var _current_floor: int = 1
+
+var _initial_position: float = 0
+var _target_position: float = 0
+
+func _ready() -> void:
+	_initial_position = global_position.y
+	door.process_mode = Node.PROCESS_MODE_DISABLED
+
+func _on_safe_zone_body_entered(_body: Node2D) -> void:
+	_is_player_safe = true
+
+func _on_safe_zone_body_exited(_body: Node2D) -> void:
+	_is_player_safe = false
+
+func _physics_process(_delta: float) -> void:
+	if not _is_player_safe or _is_moving:
+		return
+	
+	var direction := Input.get_axis("elevator_up", "elevator_down")
+	
+	if direction != 0 and _can_move_to(direction):
+		_lock_elevator()
+		_move_to(direction)
+
+func _can_move_to(direction: float):
+	var can_go_up := direction < 0 and _current_floor < highest_floor
+	var can_go_down := direction > 0 and _current_floor > lowest_floor
+	return can_go_up or can_go_down
+
+func _move_to(direction: float) -> void:
+	@warning_ignore("narrowing_conversion")
+	_current_floor -= direction
+	_target_position = global_position.y + floor_size * direction
+
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position:y", _target_position, 1)
+	tween.tween_callback(_unlock_elevator)
+
+func _lock_elevator() -> void:
+	_is_moving = true
+	door.process_mode = Node.PROCESS_MODE_INHERIT
+	
+func _unlock_elevator() -> void:
+	_is_moving = false
+	door.process_mode = Node.PROCESS_MODE_DISABLED
--- guard.gd
+++ guard.gd
@@ -1,30 +1,28 @@
-extends Node2D
+extends CharacterBody2D
 
-@export var rotation_speed: float = 3
-@export var max_rotation_angle: float = 30
+@export var rotation_speed: float = 2
+@export var max_rotation_angle: float = 10
 @export var observation_time: float = .5
 
 @onready var pivot: Node2D = $Pivot
-@onready var ray_cast: RayCast2D = $RayCast
+@onready var ray_cast: RayCast2D = $Pivot/Area2D/RayCast
 
 var tween: Tween
 
 func _ready() -> void:
-	var rotation_points = [-max_rotation_angle, max_rotation_angle]
-	
-	rotation_points.shuffle()
-	
 	tween = create_tween().set_loops()
-	tween.tween_property(pivot, 'rotation_degrees', rotation_points[0], rotation_speed)
+	tween.tween_property(pivot, 'rotation_degrees', max_rotation_angle, rotation_speed)
 	tween.tween_interval(observation_time)
-	tween.tween_property(pivot, 'rotation_degrees', rotation_points[1], rotation_speed)
+	tween.tween_property(pivot, 'rotation_degrees', -max_rotation_angle, rotation_speed)
 	tween.tween_interval(observation_time)
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	ray_cast.target_position = (ray_cast.to_local(body.global_position) - ray_cast.position) * ray_cast.transform
 	ray_cast.force_raycast_update()
 	
 	if ray_cast.is_colliding() and ray_cast.get_collider() is Player:
+		#tween.pause()
 		print("You're spotted")
 	else:
 		print("object")
